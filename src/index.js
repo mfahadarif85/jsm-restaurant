f96997ee1a64d4da4280e6521487801b740b720a
mfahadarif85 <mfahadarif@gmail.com>
2023-01-24 12:13:16 +0500

    app initialization, folder structure & navbar

---
+++ src/index.js
@@ -0,0 +1,9 @@
+import ReactDOM from "react-dom/client";
+
+import "./index.css";
+import App from "./App";
+
+const rootNode = document.getElementById("root");
+const root = ReactDOM.createRoot(rootNode);
+
+root.render(<App />);
